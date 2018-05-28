0ff24ec186f3acf09eaf2abc11adf0f84680f482
Brenden <brenden.niedermeyer@gmail.com>
2018-05-28 10:11:58 -0700

    Add mat badge and click handler to counter-button.component.ts

--- projects/my-new-library/src/lib/counter-button/counter-button.component.ts
+++ projects/my-new-library/src/lib/counter-button/counter-button.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./counter-button.component.css']
 })
 export class CounterButtonComponent implements OnInit {
+  clickCount = 0;
+  visible = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  handleButtonClick() {
+    this.clickCount++;
+    this.visible = true;
   }
-
 }
--- projects/my-new-library/src/lib/my-lib.module.ts
+++ projects/my-new-library/src/lib/my-lib.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
 
 import { CounterButtonComponent } from './counter-button/counter-button.component';
 
